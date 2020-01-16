7da4e180ef5a90291668f391a088c036ccf341f2
Daren Ace <daren.ace@gmail.com>
2020-01-16 18:03:57 +0300

    Добавлена функция поиска уникального селектора getPath (ДЗ-3).

---
+++ javascript/javascript-3.js
@@ -0,0 +1,41 @@
+const getPath = element => {
+    if (element === null || element === undefined || !element instanceof HTMLElement) {
+        throw `Illegal argument: ${element}`;
+    }
+    const elements = [];
+    while (element.parentElement !== null) {
+        const parent = element.parentElement;
+        const number = Array.prototype.indexOf.call(parent.children, element) + 1;
+        elements.push(`${element.tagName}:nth-child(${number})`);
+        element = parent;
+    }
+    elements.push(element.tagName);
+    return elements.reverse().join(" > ");
+};
+
+const test = id => {
+    const element = document.getElementById(id);
+    try {
+        const path = getPath(element);
+        const foundByPath = document.querySelector(path).getAttribute("id") === id;
+        const foundUnique = document.querySelectorAll(path).length === 1;
+        console.log(`Test element with id = ${id} is passed: ${foundByPath && foundUnique}.`);
+    } catch (error) {
+        const isErrorCorrect = error === `Illegal argument: ${element}`;
+        console.log(`Test element with id = ${id} is passed: ${isErrorCorrect}.`);
+    }
+};
+
+const runTests = () => {
+    test("0");
+    test("3");
+    test("7");
+    test("13");
+    test("17");
+    test("18");
+    test("24");
+    test("26");
+    test(null);
+    test();
+    test(3);
+};
