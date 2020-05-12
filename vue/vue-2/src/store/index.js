9a2f83415a54d60cbdda01a66a46d251b7f0378b
Daren Ace <daren.ace@gmail.com>
2020-05-12 10:40:00 +0300

    ДЗ-13. Реализован второй экран, настроены переходы по страницам.

--- vue/vue-2/src/store/index.js
+++ vue/vue-2/src/store/index.js
@@ -1,89 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import state from './state'
+import getters from './getters'
+import mutations from './mutations'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    statistics: {
-      day: 24,
-      accuracy: 0.8,
-      lastResult: {
-        total: 25,
-        solved: 10
-      }
-    },
-    settings: {
-      duration: 7,
-      difficulty: 5,
-      operations: {
-        addition: true,
-        subtraction: false,
-        multiplication: false,
-        division: true,
-        exponentiation: true
-      }
-    },
-    game: {
-      total: 25,
-      solved: 10
-    }
-  },
-  mutations: {
-    setStatistics (state, statistics) {
-      state.statistics = statistics
-    },
-
-    setSettings (state, settings) {
-      state.settings = settings
-    },
-
-    setDay (state, day) {
-      state.statistics.day = day
-    },
-
-    setAccuracy (state, accuracy) {
-      state.statistics.accuracy = accuracy
-    },
-
-    setTotal (state, total) {
-      state.statistics.lastResult.total = total
-    },
-
-    setSolved (state, solved) {
-      state.statistics.lastResult.solved = solved
-    },
-
-    setDuration (state, duration) {
-      state.settings.duration = duration
-    },
-
-    setDifficulty (state, difficulty) {
-      state.settings.difficulty = difficulty
-    },
-
-    setAddition (state, addition) {
-      state.settings.operations.addition = addition
-    },
-
-    setSubtraction (state, subtraction) {
-      state.settings.operations.subtraction = subtraction
-    },
-
-    setMultiplication (state, multiplication) {
-      state.settings.operations.multiplication = multiplication
-    },
-
-    setDivision (state, division) {
-      state.settings.operations.division = division
-    },
-
-    setExponentiation (state, exponentiation) {
-      state.settings.operations.exponentiation = exponentiation
-    }
-  },
-  actions: {
-  },
-  modules: {
-  }
+  plugins: [createPersistedState({
+    paths: [
+      'settings',
+      'statistics'
+    ]
+  })],
+  state,
+  getters,
+  mutations
 })
--- vue/vue-2/src/views/Game.vue
+++ vue/vue-2/src/views/Game.vue
@@ -3,73 +3,127 @@
     <div class="card-body">
       <header class="game-header pb-4">
         <router-link to="/" class="btn btn-outline-primary cancel-button">Отмена</router-link>
-        <div class="timer">4:37</div>
+        <timer :timeLeft="secondsLeft" :timeTotal="settings.duration" />
       </header>
-      <div class="quiz pb-4">
-        <span id="operand-1">13</span>
-        <span id="operation-1" class="math-sign">*</span>
-        <span id="operand-2" class="answer">__</span>
-        <span id="operation-2" class="math-sign">*</span>
-        <span id="operand-3" class="answer">__</span>
-        <span class="math-sign">=</span>
-        <span id="result">84 240</span>
-      </div>
-      <div class="buttons">
-        <div class="button-wrapper">
-          <button class="button">1</button>
-        </div>
-        <div class="button-wrapper">
-          <button class="button">2</button>
-        </div>
-        <div class="button-wrapper">
-          <button class="button">3</button>
-        </div>
-        <div class="button-wrapper">
-          <button class="button bg-grey">&lt;</button>
-        </div>
-        <div class="button-wrapper">
-          <button class="button">4</button>
-        </div>
-        <div class="button-wrapper">
-          <button class="button">5</button>
-        </div>
-        <div class="button-wrapper">
-          <button class="button">6</button>
-        </div>
-        <div class="button-wrapper">
-          <button class="button bg-grey">&gt;</button>
-        </div>
-        <div class="button-wrapper">
-          <button class="button">7</button>
-        </div>
-        <div class="button-wrapper">
-          <button class="button">8</button>
-        </div>
-        <div class="button-wrapper">
-          <button class="button">9</button>
-        </div>
-        <div class="button-wrapper">
-          <button class="button bg-grey">?</button>
-        </div>
-        <div class="button-wrapper wide-button">
-          <button class="button">0</button>
-        </div>
-        <div class="button-wrapper">
-          <button class="button bg-grey">=</button>
-        </div>
-      </div>
+      <quiz :userInput="userInput" :isAnswerCorrect="isUserAnswerCorrect" />
+      <keyboard @key-pressed="handleKeyPress" />
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapMutations } from 'vuex'
+import Timer from '../components/Timer'
+import Quiz from '../components/Quiz'
+import Keyboard from '../components/Keyboard'
+import gameService from '../utils/game.service'
+
 export default {
   name: 'Game',
+
+  components: {
+    Timer,
+    Quiz,
+    Keyboard
+  },
+
+  data () {
+    return {
+      timer: null,
+      secondsLeft: 0,
+      userInput: null,
+      isUserAnswerCorrect: null
+    }
+  },
+
   computed: {
-    duration: {
-      get () {
-        return this.$store.state.settings.duration
+    ...mapGetters({
+      quiz: 'getQuiz',
+      settings: 'getSettings'
+    })
+  },
+
+  methods: {
+    ...mapMutations([
+      'registerSolvedQuiz',
+      'registerUnsolvedQuiz',
+      'resetLastResult',
+      'setQuiz',
+      'updateLastGameDate',
+      'updateOverallResult',
+      'updateTotalDays'
+    ]),
+
+    startGame () {
+      this.updateTotalDays()
+      this.updateLastGameDate()
+      this.resetLastResult()
+      this.setQuiz(gameService.generateQuiz(this.settings))
+      this.secondsLeft = this.settings.duration
+      this.timer = setInterval(() => {
+        this.secondsLeft--
+        if (this.secondsLeft <= 0) {
+          this.stopGame()
+          this.$router.push({ name: 'Home' })
+        }
+      }, 1000)
+    },
+
+    stopGame () {
+      clearInterval(this.timer)
+      this.timer = null
+      this.updateOverallResult()
+    },
+
+    handleKeyPress (keyValue) {
+      switch (keyValue) {
+        case 'erase':
+          if (this.userInput) {
+            this.userInput = this.userInput < 10 ? null : Math.floor(this.userInput / 10)
+          }
+          break
+        case 'clear':
+          this.userInput = null
+          break
+        case 'next':
+          this.userInput = this.quiz.answer
+          this.registerUnsolvedQuiz()
+          setTimeout(() => {
+            this.setQuiz(gameService.generateQuiz(this.settings))
+            this.userInput = null
+          }, 500)
+          break
+        case 'check':
+          if (this.userInput && this.userInput === this.quiz.answer) {
+            this.registerSolvedQuiz()
+            this.isUserAnswerCorrect = true
+            setTimeout(() => {
+              this.setQuiz(gameService.generateQuiz(this.settings))
+              this.userInput = null
+              this.isUserAnswerCorrect = null
+            }, 500)
+          } else {
+            this.isUserAnswerCorrect = false
+            setTimeout(() => { this.isUserAnswerCorrect = null }, 500)
+          }
+          break
+        default:
+          if (!this.userInput) {
+            this.userInput = parseInt(keyValue)
+          } else if (this.userInput.toString().length < this.quiz.answer.toString().length) {
+            this.userInput = this.userInput * 10 + parseInt(keyValue)
+          }
       }
+    }
+  },
+
+  mounted () {
+    this.startGame()
+  },
+
+  destroyed () {
+    if (this.timer) {
+      this.stopGame()
     }
   }
 }
@@ -83,77 +137,5 @@ export default {
 
 .cancel-button {
   min-width: 6rem;
-}
-
-.timer {
-  width: 6rem;
-  padding: .375rem .75rem;
-  border: 1px solid #007bff;
-  border-radius: .25rem;
-  text-align: center;
-  vertical-align: middle;
-  font-size: 1rem;
-  line-height: 1.5;
-  font-weight: bold;
-  color: grey;
-  background: linear-gradient(90deg, rgba(0, 0, 0, 0.1) 80%, #007bff 80%);
-}
-
-.quiz {
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  align-items: center;
-  flex-grow: 1;
-  font-size: 2rem;
-  font-weight: bold;
-  color: grey;
-}
-
-.math-sign {
-    padding-left: .75rem;
-    padding-right: .75rem;
-}
-
-.answer {
-    color: #2c3e50;
-}
-
-.buttons {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 15px;
-}
-
-.button-wrapper {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: 5.5rem;
-}
-
-.button {
-  width: 3rem;
-  height: 3rem;
-  border: none;
-  border-radius: 50%;
-  margin: .75rem;
-  outline: none;
-  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.6);
-  background: darkorange;
-  font-size: 1.5rem;
-  font-weight: bold;
-}
-
-.button:active {
-  box-shadow: inset 0px 5px 10px 0px rgba(0, 0, 0, 0.2);
-}
-
-.wide-button {
-  grid-column: 1 / 4;
-}
-
-.bg-grey {
-  background: rgba(0, 0, 0, 0.4);
 }
 </style>
