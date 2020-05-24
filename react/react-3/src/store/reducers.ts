aa502cc210e7459d90bcab8c6d26e1ae852bc02a
Daren Ace <daren.ace@gmail.com>
2020-05-24 14:13:24 +0300

    ДЗ-11. Исправления по результатм проверки: удалён закомментированный код.

--- react/react-3/src/store/reducers.ts
+++ react/react-3/src/store/reducers.ts
@@ -1,32 +1,10 @@
-import { combineReducers } from "redux";
-import { CityAction, CurrentWeather, WeatherAction, WeatherForecast, AppAction } from "../types";
+import { AppAction } from "../types";
 import {
     ADD_CITY_TO_LIST,
     REMOVE_CITY_FROM_LIST,
     SELECT_CITY_FROM_LIST,
-    CURRENT_WEATHER_HAS_BEEN_UPDATED,
-    WEATHER_FORECAST_HAS_BEEN_UPDATED,
 } from "./actions";
 import { AppState } from "../types";
-
-// const initialCitiesState: CitiesState = {
-//     selected: null,
-//     favorites: []
-// };
-
-// const initialWeatherState: WeatherState = {
-//     current: {
-//         city: "",
-//         temperature: null,
-//         pressure: null,
-//         humidity: null,
-//         wind: {
-//             speed: null,
-//             degrees: null
-//         }
-//     },
-//     forecast: null
-// };
 
 export const initialState: AppState = {
     cities: [],
@@ -78,74 +56,3 @@ export const rootReducer = (
             return state;
     }
 };
-
-// export const cityReducer = (
-//     state: CitiesState = initialCitiesState,
-//     action: CityAction
-// ): CitiesState => {
-//     switch (action.type) {
-//         case ADD_CITY_TO_LIST:
-//             const cityName: string = action.payload as string;
-//             const exists =
-//                 state.favorites.findIndex((city) => city.name === cityName) >
-//                 -1;
-//             if (!exists) {
-//                 const id =
-//                     state.favorites.reduce(
-//                         (max, city) => Math.max(max, city.id),
-//                         Number.MIN_SAFE_INTEGER
-//                     ) + 1;
-//                 return {
-//                     ...state,
-//                     favorites: [
-//                         ...state.favorites,
-//                         {
-//                             id,
-//                             name: action.payload as string,
-//                         },
-//                     ],
-//                 };
-//             } else {
-//                 return state;
-//             }
-//         case REMOVE_CITY_FROM_LIST:
-//             return {
-//                 ...state,
-//                 favorites: state.favorites.filter(
-//                     (city) => city.id !== (action.payload as number)
-//                 ),
-//             };
-//         case SELECT_CITY_FROM_LIST:
-//             return {
-//                 ...state,
-//                 selected: action.payload as string,
-//             };
-//         default:
-//             return state;
-//     }
-// };
-
-// export const weatherReducer = (
-//     state: WeatherState = initialWeatherState,
-//     action: WeatherAction
-// ): WeatherState => {
-//     switch (action.type) {
-//         case CURRENT_WEATHER_HAS_BEEN_UPDATED:
-//             return {
-//                 ...state,
-//                 current: action.payload as CurrentWeather,
-//             };
-//         case WEATHER_FORECAST_HAS_BEEN_UPDATED:
-//             return {
-//                 ...state,
-//                 forecast: action.payload as WeatherForecast,
-//             };
-//         default:
-//             return state;
-//     }
-// };
-
-// export const rootReducer = combineReducers({
-//     cities: cityReducer,
-//     weather: weatherReducer
-// });
